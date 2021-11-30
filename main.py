2154989001bb21d93ab02d51cf683a4aef64de2a
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-11-30 07:18:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+def image():
+
+  datne = "siiii.png"
+
+  with Image.open(datne)as in:
+
+    
+    print(datne, in.format, f*{in.size}x{in.mode}*)
+    in.show()
+
+    izmers=(100,100)
+
+    im.save("small_siiii.png", im.format)
+
+
+bilde()
